d8b99dbf9172f8577a398ba30197685797af218c
spl <splatedk@gmail.com>
2026-09-17 20:15:13 +0800

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
+    }
+}
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.22" apply false
+}
